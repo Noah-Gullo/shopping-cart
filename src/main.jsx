c50a1bebd938ba4993ae0c8f89a41d5e1ec72e59
Noah Gullo <noahgulloingram@gmail.com>
2026-07-18 19:06:04 -0700

    feat: add the three routes named home, shop, and cart

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./Home";
+import Shop from "./Shop";
+import Cart from "./Cart";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path:  "/shop",
+    element: <Shop />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  }
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
